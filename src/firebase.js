4880cb08a46663f38d4575bb4826681fc9aa596e
Alan Pereira <allantcn1@gmail.com>
2026-05-02 12:59:49 -0300

    debug: log firebase api key

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 const firebaseConfig = {
-  console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -13,6 +14,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
